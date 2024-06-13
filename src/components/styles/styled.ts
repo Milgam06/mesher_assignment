29aecb66f8ffb3d23c96b46efec2242cfc117782
Milgam06 <milgamfruit@gmail.com>
2024-06-13 21:13:36 +0900

    feat(component): separate Styles what frequently used into Component

---
+++ src/components/styles/styled.ts
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+export const SpaceBetweenContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const CenterContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
