2beb079dd988712565d3e3210cdc6b11059637c5
MILGAM <93873127+Milgam06@users.noreply.github.com>
2024-06-16 05:02:53 +0900

    feat(component): segment nodes to use each thing

--- src/components/common/swappingTokenBox/index.tsx
+++ src/components/common/swappingTokenBox/index.tsx
@@ -1,26 +1,20 @@
-import { Input, ChoiceTokenButton, Text } from "@/components";
-
 import * as S from "./styled";
 
-export const SwappingTokenBox: React.FC = () => {
+export interface SwappingTokenBoxProps {
+  children: React.ReactNode;
+}
+
+export const SwappingTokenOverlay: React.FC<SwappingTokenBoxProps> = ({
+  children,
+}) => {
   return (
     <>
-      <S.SwappingTokenContainer>
-        <S.SwappingTokenInputContainer>
-          <Input placeholder="0.0" inputType="number" />
-          <ChoiceTokenButton />
-        </S.SwappingTokenInputContainer>
-        <S.SwappingTokenDollarValueContainer>
-          <Text size={1} weight={300} colors="#B2B9D0">
-            asdf
-          </Text>
-        </S.SwappingTokenDollarValueContainer>
-      </S.SwappingTokenContainer>
+      <S.SwappingTokenContainer>{children}</S.SwappingTokenContainer>
     </>
   );
 };
 
-// export const SwappingTokenBox = Object.assign(S.SwappingTokenContainer, {
-//   TokenInput: S.SwappingTokenInputContainer,
-//   TokenDollarValue: S.SwappingTokenDollarValueContainer,
-// });
+export const SwappingTokenBox = Object.assign(SwappingTokenOverlay, {
+  TokenInput: S.SwappingTokenInputContainer,
+  TokenDollarValue: S.SwappingTokenDollarValueContainer,
+});
