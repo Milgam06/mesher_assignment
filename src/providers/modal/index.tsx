3c97192863a82af09030a0882ec9eb3c419c1754
MILGAM <93873127+Milgam06@users.noreply.github.com>
2024-06-15 19:10:50 +0900

    feat(provider): add ModalProvider

---
+++ src/providers/modal/index.tsx
@@ -0,0 +1,39 @@
+import { createContext, useContext, useState } from "react";
+import { Modal, ModalProps } from "@/components";
+
+export interface ModalContextProps {
+  open: (props: ModalProps) => void;
+  close: () => void;
+}
+
+export const ModalContext = createContext<ModalContextProps | null>(null);
+
+export const ModalProvider: React.FC<ModalProps> = ({ children }) => {
+  const [modal, setModal] = useState<ModalProps | undefined>(undefined);
+  const open = (props: ModalProps) => {
+    setModal(props);
+  };
+  const close = () => {
+    setModal(undefined);
+  };
+  return (
+    <>
+      <ModalContext.Provider value={{ open, close }}>
+        {modal !== undefined && (
+          <Modal.Overlay>
+            <Modal {...modal} />
+          </Modal.Overlay>
+        )}
+        {children}
+      </ModalContext.Provider>
+    </>
+  );
+};
+
+export const useModal = () => {
+  const context = useContext(ModalContext);
+  if (!context)
+    throw new Error("useModal() must be used within a ModalProvider");
+
+  return context;
+};
