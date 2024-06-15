cc67b76a7c5bcfdb3d16085121033cd5ad16ff89
MILGAM <93873127+Milgam06@users.noreply.github.com>
2024-06-16 04:55:34 +0900

    feat(component): add width, height, isFontLarge types in inputProps to resize InputComponent (I don't like this fetch)

--- src/components/common/input/index.tsx
+++ src/components/common/input/index.tsx
@@ -1,20 +1,31 @@
 import { HTMLInputTypeAttribute } from "react";
 import * as S from "./styled";
 
-export interface InputProps {
+export interface InputProps extends S.InputStyleProps {
   placeholder: string;
   inputType: HTMLInputTypeAttribute;
   value?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export const Input: React.FC<InputProps> = ({ placeholder, inputType }) => {
+export const Input: React.FC<InputProps> = ({
+  placeholder,
+  inputType,
+  width,
+  height,
+  isFontLarge,
+  onChange,
+}) => {
   return (
     <>
       <S.InputWrapper
         placeholder={placeholder}
+        width={width}
+        height={height}
+        isFontLarge={isFontLarge}
         type={inputType}
         pattern={inputType === "number" ? "^d+(.d{1,10})?$" : undefined}
+        onChange={onChange}
         onWheel={(event) => (event.target as HTMLElement).blur()}
         step={inputType === "number" ? "0.0000000001" : undefined}
       />
--- src/components/common/input/styled.ts
+++ src/components/common/input/styled.ts
@@ -1,13 +1,19 @@
 import styled from "@emotion/styled";
 
-export const InputWrapper = styled.input`
-  width: 22rem;
-  height: 4rem;
+export interface InputStyleProps {
+  width: string;
+  height: string;
+  isFontLarge?: boolean;
+}
+
+export const InputWrapper = styled.input<InputStyleProps>`
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
   border: none;
   background-color: transparent;
-  font-size: 3rem;
+  font-size: ${(props) => (props.isFontLarge ? "3rem" : "1rem")};
   color: #fff;
-  letter-spacing: 0.1rem;
+  letter-spacing: 0.01rem;
   ::placeholder {
     color: #b2b9d0;
   }
