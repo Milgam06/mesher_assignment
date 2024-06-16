b9c867bb7e9d37c1792ce6cf562a4a1c9c35b41a
MILGAM <93873127+Milgam06@users.noreply.github.com>
2024-06-17 06:45:47 +0900

    feat: add react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { useQuery } from "react-query";
 import { Global } from "@emotion/react";
 import { globalStyle } from "@/styles";
-import { ModalProvider } from "./providers";
+import { ModalProvider } from "@/providers";
+import { getList } from "@/api";
+import { useTokenStore } from "@/stores";
 
 import { Router } from "./router";
 
 export const App: React.FC = () => {
+  const { setTokenList } = useTokenStore();
+  useQuery("tokenList", getList, {
+    onSuccess: (data) => {
+      setTokenList(data);
+    },
+    onError: (error) => {
+      console.error("Failed to fetch token list:", error);
+    },
+  });
   return (
     <>
       <Global styles={globalStyle} />
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 import App from "./App.tsx";
-
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </QueryClientProvider>
 );
